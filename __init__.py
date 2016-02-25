53a2830bbd7890d8e4fa388aa2b7370af9d498fa
David Hoffman <dave.p.hoffman@gmail.com>
2016-02-24 20:27:36 -0500

    Scale takes dtype as an argument

--- __init__.py
+++ __init__.py
@@ -6,9 +6,10 @@ This is for small utility functions that don't have a proper home yet
 
 import numpy as np
 from scipy.fftpack import ifftshift, fftshift, fftn
+from scipy.signal import fftconvolve, convolve
 
 
-def scale(data):
+def scale(data, dtype=None):
     '''
     Scales data to 0 to 1 range
 
@@ -21,12 +22,28 @@ def scale(data):
     1.0
     >>> b.min()
     0.0
+    >>> b = scale(a, dtype = np.uint16)
+    >>> b.max()
+    65535
+    >>> b.min()
+    0
     '''
 
     dmin = data.min()
     dmax = data.max()
 
-    return (data-dmin)/(dmax-dmin)
+    if dtype is None:
+        tmin = 0.0
+        tmax = 1.0
+    else:
+        if np.issubdtype(dtype, np.integer):
+            tmin = np.iinfo(dtype).min
+            tmax = np.iinfo(dtype).max
+        else:
+            tmin = np.finfo(dtype).min
+            tmax = np.finfo(dtype).max
+
+    return ((data - dmin)/(dmax - dmin)*(tmax - tmin) + tmin).astype(dtype)
 
 
 def scale_uint16(data):
@@ -534,3 +551,69 @@ class Pupil(object):
     def gen_otf(self, **kwargs):
         raise NotImplementedError
         # need to implement this function which returns OTFs
+
+
+def richardson_lucy(image, psf, iterations=50, clip=False):
+    """Richardson-Lucy deconvolution.
+    Parameters
+    ----------
+    image : ndarray
+       Input degraded image (can be N dimensional).
+    psf : ndarray
+       The point spread function.
+    iterations : int
+       Number of iterations. This parameter plays the role of
+       regularisation.
+    clip : boolean, optional
+       True by default. If true, pixel value of the result above 1 or
+       under -1 are thresholded for skimage pipeline compatibility.
+    
+    Returns
+    -------
+    im_deconv : ndarray
+       The deconvolved image.
+    
+    Examples
+    --------
+    >>> from skimage import color, data, restoration
+    >>> camera = color.rgb2gray(data.camera())
+    >>> from scipy.signal import convolve2d
+    >>> psf = np.ones((5, 5)) / 25
+    >>> camera = convolve2d(camera, psf, 'same')
+    >>> camera += 0.1 * camera.std() * np.random.standard_normal(camera.shape)
+    >>> deconvolved = restoration.richardson_lucy(camera, psf, 5)
+
+    References
+    ----------
+    .. [1] http://en.wikipedia.org/wiki/Richardson%E2%80%93Lucy_deconvolution
+    """
+    # Stolen from the dev branch of skimage because stable branch is slow
+    # compute the times for direct convolution and the fft method. The fft is of
+    # complexity O(N log(N)) for each dimension and the direct method does
+    # straight arithmetic (and is O(n*k) to add n elements k times)
+    direct_time = np.prod(image.shape + psf.shape)
+    fft_time = np.sum([n*np.log(n) for n in image.shape + psf.shape])
+
+    # see whether the fourier transform convolution method or the direct
+    # convolution method is faster (discussed in scikit-image PR #1792)
+    time_ratio = 40.032 * fft_time / direct_time
+
+    if time_ratio <= 1 or len(image.shape) > 2:
+        convolve_method = fftconvolve
+    else:
+        convolve_method = convolve
+
+    image = image.astype(np.float)
+    psf = psf.astype(np.float)
+    im_deconv = 0.5 * np.ones(image.shape)
+    psf_mirror = psf[::-1, ::-1]
+
+    for _ in range(iterations):
+        relative_blur = image / convolve_method(im_deconv, psf, 'same')
+        im_deconv *= convolve_method(relative_blur, psf_mirror, 'same')
+
+    if clip:
+        im_deconv[im_deconv > 1] = 1
+        im_deconv[im_deconv < -1] = -1
+
+    return im_deconv
