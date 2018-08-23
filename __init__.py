f3f894bf8337dd25bd0442ba856221aec68009ee
David Hoffman <dave.p.hoffman@gmail.com>
2018-08-23 10:15:05 -0400

    add power law functions

--- __init__.py
+++ __init__.py
@@ -263,7 +263,6 @@ def slice_maker(xs, ws):
     return tuple(toreturn)
 
 
-
 def fft_pad(array, newshape=None, mode='median', **kwargs):
     """Pad an array to prep it for fft"""
     # pull the old shape
@@ -563,6 +562,7 @@ def fft_gaussian_filter(img, sigma):
 #     data = pd.read_csv(path)
 #     
 
+
 def multi_exp(xdata, *args):
     """Power and exponent"""
     odd = len(args) % 2
@@ -575,6 +575,24 @@ def multi_exp(xdata, *args):
         a, k = args[i:i + 2]
         res += a * np.exp(-k * xdata)
     return res
+
+
+def multi_exp_jac(xdata, *args):
+    """Power and exponent jacobian"""
+    odd = len(args) % 2
+
+    tostack = []
+
+    for i in range(0, len(args) - odd, 2):
+        a, k = args[i:i + 2]
+        tostack.append(np.exp(-k * xdata))
+        tostack.append(-a * xdata * tostack[-1])
+
+    if odd:
+        # there's an offset
+        tostack.append(np.ones_like(xdata))
+
+    return np.vstack(tostack).T
 
 
 def exponent(xdata, amp, rate, offset):
@@ -596,13 +614,16 @@ def _estimate_exponent_params(data, xdata):
         amp, rate, offset = _estimate_exponent_params(-data, xdata)
         return np.array((-amp, rate, -offset))
 
+
 def _estimate_components(data, xdata):
     """"""
     raise NotImplementedError
 
+
 def exponent_fit(data, xdata=None, offset=True):
     """Utility function that fits data to the exponent function"""
     return multi_exp_fit(data, xdata, components=1, offset=offset)
+
 
 def multi_exp_fit(data, xdata=None, components=None, offset=True):
     """Utility function that fits data to the exponent function
@@ -662,6 +683,89 @@ def multi_exp_fit(data, xdata=None, components=None, offset=True):
         return curve_fit(multi_exp, xdata_fixed, data_fixed, p0=pguess, maxfev=2000)
     else:
         raise RuntimeError("Not enough good points to fit.")
+
+
+def estimate_power_law(x, y, diagnostics=False):
+    """Estimate the best fit parameters for a power law by linearly fitting the loglog plot"""
+    # can't take log of negative points
+    valid_points = y > 0
+    # pull valid points and take log
+    xx = np.log(x[valid_points])
+    yy = np.log(y[valid_points])
+    # weight by sqrt of value, make sure we get the trend right
+    w = np.sqrt(y[valid_points])
+    # fit line to loglog
+    neg_b, loga = np.polyfit(np.log(x[valid_points]), np.log(y[valid_points]), 1, w=w)
+    if diagnostics:
+        plt.loglog(x[valid_points], y[valid_points])
+        plt.loglog(x, np.exp(loga) * x ** (neg_b))
+    return np.exp(loga), -neg_b
+
+
+def _test_pow_law(popt, xmin):
+    """Utility function for testing power law params"""
+    a, b = popt
+    assert a > 0, "Scale invalid"
+    assert b > 1, "Exponent invalid"
+    assert xmin > 0, "xmin invalid"
+
+
+def power_percentile(p, popt, xmin=1):
+    """Percentile of a single power law function"""
+    assert 0 <= p <= 1, "percentile invalid"
+    _test_pow_law(popt, xmin)
+    a, b = popt
+    x0 = (1 - p) ** (1 / (1 - b)) * xmin
+    return x0
+
+
+def power_percentile_inv(x0, popt, xmin=1):
+    """Given an x value what percentile of the power law function does it correspond to"""
+    _test_pow_law(popt, xmin)
+    a, b = popt
+    p = 1 - (x0 / xmin) ** (1 - b)
+    return p
+
+
+def power_intercept(popt, value=1):
+    """At what x value does the function reach value"""
+    a, b = popt
+    assert a > 0, "a = {}".format(value)
+    assert value > 0, "value = {}".format(value)
+    return (a / value) ** (1 / b)
+
+
+def power_law(xdata, *args):
+    """An multi-power law function"""
+    odd = len(args) % 2
+    if odd:
+        offset = float(args[-1])
+    else:
+        offset = 0.0
+    res = np.ones_like(xdata) * offset
+    lx = np.log(xdata)
+    for i in range(0, len(args) - odd, 2):
+        res += args[i] * np.exp(-args[i + 1] * lx)
+    return res
+
+
+def power_law_jac(xdata, *args):
+    """Jacobian for a multi-power law function"""
+    odd = len(args) % 2
+    tostack = []
+    lx = np.log(xdata)
+    for i in range(0, len(args) - odd, 2):
+        a, b = args[i:i + 2]
+        # dydai
+        tostack.append(np.exp(-b * lx))
+        # dydki
+        tostack.append(-lx * a * tostack[-1])
+
+    if odd:
+        # there's an offset
+        tostack.append(np.ones_like(xdata))
+
+    return np.vstack(tostack).T
 
 
 def get_tif_urls(baseurl):
